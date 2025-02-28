57ede17f2926b57f2b18e73b350add381d776e56
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2025-02-28 19:27:40 -0300

    Componente HeroSection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HerosectionComponent } from './components/herosection/herosection.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HerosectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HerosectionComponent } from './components/herosection/herosection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HerosectionComponent
   ],
   imports: [
     BrowserModule,
